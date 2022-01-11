65ff662b9e62390329d26e5f3ae3b3711474e3ab
Carlos Arguello <carguello0210@gmail.com>
2022-01-10 20:20:15 -0500

    Refactor axios instance

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,3 +4,14 @@ export const request = axios.create({
   baseURL: 'https://pruebas-muy-candidatos.s3.us-east-2.amazonaws.com/',
   timeout: 4000,
 });
+
+request.interceptors.response.use(
+  ({ data, status, statusText }) => {
+    if (status === 200 && statusText === 'OK') {
+      return data.response;
+    }
+  },
+  (error) => {
+    throw error;
+  }
+);
