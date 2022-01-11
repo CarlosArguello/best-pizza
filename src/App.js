670eaae6b17be6e53d520a8bd2b4f251a4a67217
Carlos Arguello <carguello0210@gmail.com>
2022-01-10 20:24:01 -0500

    Create Store component & route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Login } from 'components/login/Login';
+import { Stores } from 'components/stores/Stores';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
+      <Route exact path="/" element={<Stores />} />
       <Route exact path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
